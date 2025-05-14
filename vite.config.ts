83c96e532ee3e4a79de84656e0fdb0f56d6862e1
Nathanael Tan <nathanaeltan@Nathanaels-MacBook-Pro.local>
2025-05-14 11:55:36 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,26 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['recipe-extractor-front-end.onrender.com'],
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
